33785165ebd1f61870ecced421b2f5cdb63fb1c8
tommykenny <49381354+tommykenny@users.noreply.github.com>
2020-10-23 21:24:26 -0500

    changed build to public in index.js path

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/blackS', (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	res.sendFile(path.join(__dirname+'/client/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
